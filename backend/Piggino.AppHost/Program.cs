02d875d637bfb3202bdc41a0a0081d931800cfa8
bielborgesc <gabrielborges565@gmail.com>
2026-01-13 15:00:37 -0300

    fix asspire run

--- backend/Piggino.AppHost/Program.cs
+++ backend/Piggino.AppHost/Program.cs
@@ -1,6 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.Piggino_Api>("piggino-api");
+var apiService = builder.AddProject("piggino-api", "../Piggino.Api/Piggino.Api.csproj");
 
 builder.AddNpmApp("piggino-frontend", "../../frontend", "dev")
     .PublishAsDockerFile();
